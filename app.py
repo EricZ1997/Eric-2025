267ba2604aa183814a688d688da1ef527c459586
EricZ1997 <zhangxiao001@yahoo.com>
2025-05-21 17:58:51 +0900

    Update app.py

--- app.py
+++ app.py
@@ -201,5 +201,8 @@ def close_account():
     save_users(users)
     return jsonify({"message": "Account and user successfully removed"}), 200
 
+import os
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
